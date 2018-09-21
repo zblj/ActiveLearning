3fc840da224fc3012237ea4b3d85c58e085da216
Mirco Meiners <Mirco.Meiners@hs-bremen.de>
2018-09-21 13:51:35 +0200

    Added new Fig. in Activity 01, Fig. 02

--- activelearning-de/source/img/Activity_01_Fig_02.py
+++ activelearning-de/source/img/Activity_01_Fig_02.py
@@ -8,19 +8,21 @@ import matplotlib.pyplot as plt
 
 # Define sine wave
 A = 1
-theta = 0
+theta = 0.5*np.pi
 f = 1
 w = 2*np.pi*f
 t = np.linspace(0, 2, 100)
-v1 = A * np.sin(w*t + theta)
+v1 = A * np.sin(w*t)
+v2 = A * np.sin(w*t + theta)
 
 # Plot
 plt.figure(1)
-plt.plot(t, v1)
+plt.plot(t, v1, label=r'$\theta=0$')
+plt.plot(t, v2, label=r'$\theta=\frac{\pi}{2}$')
 plt.xlabel(r'Zeit t in ms')
 plt.ylabel(r'Amplitude A')
 plt.grid()
 plt.legend()
 
 # plt.show()
-plt.savefig('Activity_01_Fig_01.png')
+plt.savefig('Activity_01_Fig_02.png')
