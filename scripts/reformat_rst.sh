36b4a34d318971fbab0f403c135dd46e945be2c7
Mirco Meiners <Mirco.Meiners@hs-bremen.de>
2018-09-26 12:27:07 +0200

    Cleaned repo for readthedocs.

--- scripts/reformat_rst.sh
+++
@@ -1,5 +0,0 @@
-for f in `ls Activity_*.rst`
-do
-    gsed -i -e 's/: math: `/ :math:`/g; s/Bild ::/figure::/g; s/img \//img\//g; s/Mathematik/math/g; s/Hinweis/note/g; s/_Figure_/_Fig_/g' $f
-done
-	 
