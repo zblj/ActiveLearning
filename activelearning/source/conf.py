bbbaf19fdd6034434211e94b66e9c254fa4dfcd2
n.dnailyuk <n.dnailyuk@integrasources.com>
2020-04-17 17:23:01 +0700

    Change html logo

--- activelearning/source/conf.py
+++ activelearning/source/conf.py
@@ -166,7 +166,7 @@ html_favicon = "img/favicon.ico"
 #
 # html_logo = None
 
-html_logo = "img/RedPitayaLogo.png"
+html_logo = "img/logo_stemlab.svg"
 
 # The name of an image file (relative to this directory) to use as a favicon
 # of the docs.  This file should be a Windows icon file (.ico) being 16x16 or
